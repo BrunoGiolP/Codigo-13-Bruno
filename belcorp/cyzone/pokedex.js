68229591ea1f8e07659e53de7e30463fd247baaf
BrunoGiolP <brunogiol78@gmail.com>
2022-09-08 19:23:03 -0500

    porta

---
+++ belcorp/cyzone/pokedex.js
@@ -0,0 +1,42 @@
+const containerPokemons = document.querySelector("#pokemon-card");
+const idPokemon = document.querySelector("#poke-id");
+const nombrePokemon = document.querySelector("#poke-name");
+const tipoPokemon = document.querySelector("#tipo-pokemon");
+
+const mostrarPokemon = () =>{
+    pokes.map(async (poke, index) => {
+        //console.log(`${index+1} - ${poke.name}`)
+        console.log(poke.type)
+        const html = `
+        <div class="col-md-2" >
+        <div class="card pokemon-card mt-3">
+            
+            <button type="button" class="btn btn-primary" 
+            data-bs-toggle="modal" 
+            data-bs-target="#exampleModal" 
+            onclick="showDetailPokemon(${poke.id})">
+              <img src= ${poke.img} alt="">
+            </button>
+          </div>
+          </div>
+        `;
+
+        containerPokemons.innerHTML += html;
+    });
+      
+};
+mostrarPokemon();
+
+const showDetailPokemon = (id) => {
+  const pokemon = pokes.find((poke) => poke.id === id);
+  document.getElementById("poke-img").src= pokemon.img;
+  idPokemon.innerHTML = pokemon.id;
+  nombrePokemon.innerHTML = pokemon.name;
+  //tipoPokemon.innerHTML = pokemon.type;
+  const pokemonType = pokemon.type.map((item) => `<li>${item}</li>`)
+  tipoPokemon.innerHTML = pokemonType
+};
+
+
+
+
---
+++ belcorp/cyzone/pokemones.js
@@ -0,0 +1,39 @@
+const pokes = [
+    {
+  "id": 1,
+  "img": "./../images/cyzone/CY_ADIVINA-FR_CL.gif",
+}, {
+  "id": 2,
+  "img": "./../images/cyzone/CY_ADIVINA-FR3.gif",
+}, {
+  "id": 3,
+  "img": "./../images/cyzone/CY_FASHON-SALE_CL.gif",
+}, {
+  "id": 4,
+  "img": "./../images/cyzone/CY_NOCTURNO.gif",
+}, {
+  "id": 5,
+  "img": "./../images/cyzone/CY_REF-SF.gif",
+}, {
+  "id": 6,
+  "img": "./../images/cyzone/CY_REFUERZO-MQ.gif",
+}, {
+  "id": 7,
+  "img": "./../images/cyzone/CY_SEMANA-PIEL2.gif",
+}, {
+  "id": 8,
+  "img": "./../images/cyzone/CY_RXC_MX.gif",
+}, {
+  "id": 9,
+  "img": "./../images/cyzone/CY_CYBER_PE.gif",
+}, {
+  "id": 10,
+  "img": "./../images/cyzone/CY_CYBER-CELEBRACO_PE.gif",
+}, {
+  "id": 11,
+  "img": "./../images/cyzone/CY_FRAGANCIAS.gif",
+}, {
+  "id": 12,
+  "img": "./../images/cyzone/CY_UD-CYBER_PE.gif",
+}
+];
