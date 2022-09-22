028c2da7248ba173380029360b36affb357eebe4
BrunoGiolP <brunogiol78@gmail.com>
2022-09-22 15:14:18 -0500

    porta

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
-let nombre = document.getElementById('name');
-let correo = document.getElementById('email');
-let asunto = document.getElementById('subject');
-let mensaje = document.getElementById('messaje');
+var nombre = document.getElementById('name');
+var correo = document.getElementById('email');
+var asunto = document.getElementById('subject');
+var mensaje = document.getElementById('message');
 var error = document.getElementById('Error');
 error.style.color = 'Red';
-function enviarFormulario(){
-    console.log('Enviando formulario...');
 
-    var mensajesError = [];
+// function enviarFormulario(){
+//     console.log('Enviando formulario...');
+
+//     var mensajesError = [];
     
-    if(nombre.value === null || nombre.value === ''){
-        mensajesError.push('Ingresa tu nombre');
-    }
-    if(correo.value === null || correo.value === ''){
-        mensajesError.push('Ingresa tu email');
-    }
+//     if(nombre.value === null || nombre.value === ''){
+//         mensajesError.push('Ingresa tu nombre');
+//     }
+//     if(correo.value === null || correo.value === ''){
+//         mensajesError.push('Ingresa tu email');
+//     }
 
-    if(asunto.value === null || asunto.value === ''){
-        mensajesError.push('Ingresa un asunto');
-    }
-    if(mensaje.value === null || mensaje.value === ''){
-        mensajesError.push('Ingresa un mensaje');
-    }
+//     if(asunto.value === null || asunto.value === ''){
+//         mensajesError.push('Ingresa un asunto');
+//     }
+//     if(mensaje.value === null || mensaje.value === ''){
+//         mensajesError.push('Ingresa un mensaje');
+//     }
 
-    error.innerHTML = mensajesError.join(', ');
+//     error.innerHTML = mensajesError.join(', ');
 
-    return false
+//     return false
     
-}
+// }
 
 var form = document.getElementById('formulario');
 form.addEventListener('submit', function(evt){
-    evt.presentDefault();
-    console.log('Enviando formulario...');
+    evt.preventDefault();
+         var mensajesError = [];
+    
+            if(nombre.value === null || nombre.value === ''){
+                mensajesError.push('Ingresa tu nombre');
+            }
+            if(correo.value === null || correo.value === ''){
+                mensajesError.push('Ingresa tu email');
+            }
+
+            if(asunto.value === null || asunto.value === ''){
+                mensajesError.push('Ingresa un asunto');
+            }
+            if(mensaje.value === null || mensaje.value === ''){
+                mensajesError.push('Ingresa un mensaje');
+            }
+
+            error.innerHTML = mensajesError.join(', ');
+
 })
